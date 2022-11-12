4b3fd3d602b86ad7d6246f0d6638883fa1583308
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-12 13:59:39 -0700

    complete relationship associations

--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BulkDiscount, type: :model do
   it { should belong_to :merchant }
+  it { should have_many(:items).through(:merchant) }
+  it { should have_many(:invoice_items).through(:items) }
+  it { should have_many(:invoices).through(:invoice_items) }
 end
